76e17468fdba7db4a1aff1cd96cbc8c3d38b4c71
Anwesh43 <anweshthecool0@gmail.com>
2018-10-02 17:11:26 +0530

    adding HTMNode class

--- halftrianglemoverview/src/main/java/com/anwesh/uiprojects/halftrianglemoverview/HalfTriangleMoverView.kt
+++ halftrianglemoverview/src/main/java/com/anwesh/uiprojects/halftrianglemoverview/HalfTriangleMoverView.kt
@@ -105,4 +105,47 @@ class HalfTriangleMoverView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HTMNode(var i : Int, val state : State = State()) {
+        private var next : HTMNode? = null
+        private var prev : HTMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = HTMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHTMNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HTMNode {
+            var curr : HTMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
