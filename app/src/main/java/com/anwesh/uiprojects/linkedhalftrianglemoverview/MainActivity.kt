00e93a3c4bd848712cd7cae8ba1bc520923b234f
Anwesh43 <anweshthecool0@gmail.com>
2018-10-02 17:25:40 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedhalftrianglemoverview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedhalftrianglemoverview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedhalftrianglemoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.halftrianglemoverview.HalfTriangleMoverView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        HalfTriangleMoverView.create(this)
     }
 }
