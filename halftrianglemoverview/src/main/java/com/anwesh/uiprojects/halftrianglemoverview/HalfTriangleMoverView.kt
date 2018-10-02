62f1b1971d3c68c5659bebe01b674360512800c6
Anwesh43 <anweshthecool0@gmail.com>
2018-10-02 14:52:56 +0530

    importing libraries and defining nodes

--- halftrianglemoverview/src/main/java/com/anwesh/uiprojects/halftrianglemoverview/HalfTriangleMoverView.kt
+++ halftrianglemoverview/src/main/java/com/anwesh/uiprojects/halftrianglemoverview/HalfTriangleMoverView.kt
@@ -3,3 +3,14 @@ package com.anwesh.uiprojects.halftrianglemoverview
 /**
  * Created by anweshmishra on 02/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Path
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
