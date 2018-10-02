3aae483d84cec7d15184f5819058ac688a637e8c
Anwesh43 <anweshthecool0@gmail.com>
2018-10-02 16:01:01 +0530

    adding HalfTriangleView class

--- halftrianglemoverview/src/main/java/com/anwesh/uiprojects/halftrianglemoverview/HalfTriangleMoverView.kt
+++ halftrianglemoverview/src/main/java/com/anwesh/uiprojects/halftrianglemoverview/HalfTriangleMoverView.kt
@@ -14,3 +14,21 @@ import android.content.Context
 import android.app.Activity
 
 val nodes : Int = 5
+
+class HalfTriangleMoverView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN  -> {
+
+            }
+        }
+        return true
+    }
+}
