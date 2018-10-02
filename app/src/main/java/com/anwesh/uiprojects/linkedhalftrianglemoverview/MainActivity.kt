544708b7c443a55237a23afbd45897ff91453e45
Anwesh43 <anweshthecool0@gmail.com>
2018-10-02 17:47:38 +0530

    adding  fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedhalftrianglemoverview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedhalftrianglemoverview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedhalftrianglemoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.halftrianglemoverview.HalfTriangleMoverView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         HalfTriangleMoverView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
